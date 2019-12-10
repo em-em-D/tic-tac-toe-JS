6b997b75af98c05a60df466a0ed91aeb8a842d84
em-em-D <michaeldamulak@gmail.com>
2019-12-10 10:33:18 +0100

    game-logic in progress

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const Gameboard = (() => {
      const board = document.getElementById("board");
      const boardTable = document.getElementById("board-table");     
      const sides = document.querySelectorAll('.choice span');
-     
+     const gamearray = [["1","2","3"],["4","5","6"],["7","8","9"]];
+
      sides.forEach(function(side){
          side.addEventListener('click', function(event){
                     const player1_side = event.target.textContent
@@ -11,17 +12,35 @@ const Gameboard = (() => {
                     player1_side === 'X'? player2_side = 'O': player1_side = 'X'
                     const player2 = new Player(player2_side)
                     console.log(player1, player2);
+        startGame();
 
          } )
      })
 
-    const gamearray = [["1","2","3"],["4","5","6"],["7","8","9"]];
+
+     function taketurns(){
+        // to allocate free positions to the other player
+     }
+
+     function updateBoard(side){
+        
+     }
+
+     
+     function startGame(){
+        boardTable.innerHTML = ""
+        render()
+
+     }
+
         function render(){
         for (moves of gamearray){
+            
             let tablerow = `<tr> `
             for(move of moves ){
-             tablerow +=  `<td> ${move}</td>`
-
+             movedata =  `<td> ${move}</td>`
+             tablerow +=  movedata
+            
             }
             tablerow += `</tr>`
 
